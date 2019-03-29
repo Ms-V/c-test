652cfc490c4c036d812cca14101829e5c5f163ec
我脑袋重了 <fuqi.s@outlook.com>
2019-03-29 09:58:16 +0800

    Update dianhuapanding.c
    优化代码就完事了

--- dianhuapanding.c
+++ dianhuapanding.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
-int password = 1993;//密码
-int enter;//定义输入密码
-int main()
+#include <stdbool.h>
+int password = 1993; //吸入
+int enter, pw_time;  //定义输入密码，声明全局变量pw_time用于密码输入计数
+_Bool validate;//声明布尔值validate，用于验证密码输入结果
+int main(void)
 {
     void int_put_pw();
-    printf("Please enter the password:");
+    printf("Please enter the password: ");
     int_put_pw();
+    system("pause");
 }
 
-void int_put_pw()//输入密码
+void int_put_pw() //输入密码
 {
-    int com();
-    int pw_time = 0;//设置输入次数变量
-    for (; pw_time<=3;pw_time++) //设置输入次数不能超过三回
+    void com();
+    for (pw_time = 0; pw_time <= 3; ++pw_time) //设置输入次数不能超过三回
     {
-        if(pw_time<3)
+        if (pw_time < 3)
         {
-            scanf("%d",&enter);
+            scanf("%d", &enter);
             com(enter);
-            if(com(1))
+            if (validate == true)
             {
+
                 break;
             }
             else
@@ -29,22 +32,25 @@ void int_put_pw()//输入密码
         }
         else
         {
-            printf("Over the enter upper limit!!!The firing shutdown!!");
+            printf("Over the enter upper limit!!!The firing shutdown!!\n");
             break;
         }
     }
 }
 
-int com()//对比函数
+void com() //对比函数
 {
-    if(enter==password)
+    if (enter == password)
     {
-        printf("Password is right The firing program is start.");
-        return 1;
+        printf("Password is right The firing program is start.\n");
+        validate = true;
     }
     else
     {
-        printf("the password is wrong please enter again:");
-        return 0;
+        if (pw_time < 2)
+        {
+            printf("the password is wrong please enter again: ");
+        }
+        validate = false;
     }
 }
